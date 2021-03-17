4e76537388d516d4988ed03a10eaac45a229bfe3
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-03-17 16:12:58 -0300

    Atualização 1.1

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -12,7 +12,12 @@ public class Jogo {
         LogicaDoJogo lg = new LogicaDoJogo();
 
         try {
+
+            String cor = "\u001B[";
+
+            System.out.print(cor + "32" + "m");
             System.out.println("------------------------");
+            System.out.print(cor + "m");
             System.out.println("Vamos jogar?");
             System.out.println("0 - Para encerrar");
             System.out.println("1 - Pedra");
@@ -24,23 +29,60 @@ public class Jogo {
             switch (numero) {
 
                 case 0:
+                    System.out.print(cor + "32" + "m");
                     System.out.println("------------------------");
+                    System.out.print(cor + "m");
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
                     lg.Encerrado();
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
+
                     break;
 
                 case 1:
+                    System.out.print(cor + "32" + "m");
                     System.out.println("------------------------");
+                    System.out.print(cor + "m");
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
                     lg.Pedra();
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
                     break;
 
                 case 2:
+
+                    System.out.print(cor + "32" + "m");
                     System.out.println("------------------------");
+                    System.out.print(cor + "m");
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
                     lg.Papel();
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
+
                     break;
 
                 case 3:
+
+                    System.out.print(cor + "32" + "m");
                     System.out.println("------------------------");
+                    System.out.print(cor + "m");
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
                     lg.Tesoura();
+                    System.out.print(cor + "33" + "m");
+                    System.out.println("------------------------");
+                    System.out.print(cor + "m");
+
                     break;
 
                 default:
@@ -49,11 +91,18 @@ public class Jogo {
             }
 
         } catch (Exception e) {
+
+            String cor2 = "\u001B[";
+            System.out.print(cor2 + "31" + "m");
+            System.out.println("------------------------");
+            System.out.print(cor2 + "m");
             System.out.println("Você digitou um caractere invalido!");
-        }
-        
-        finally{
-        teclado.close();
+            System.out.print(cor2 + "31" + "m");
+            System.out.println("------------------------");
+            System.out.print(cor2 + "m");
+
+        } finally {
+            teclado.close();
         }
 
     }
